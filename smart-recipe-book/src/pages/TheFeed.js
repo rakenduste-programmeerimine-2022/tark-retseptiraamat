daf2d652cf8a58d63501a70b04bb0e660bbb3739
Kairi Käpa <90192427+iriak2@users.noreply.github.com>
2022-11-25 12:30:09 +0200

    adding TheFeed.js changes

--- smart-recipe-book/src/pages/TheFeed.js
+++ smart-recipe-book/src/pages/TheFeed.js
@@ -5,6 +5,17 @@ function TheFeed() {
     return (
         <>
             <NavLoggedOut />
+            <Grid sx={{display: "grid", gridTemplateColumns: "repeat(auto-fill, 350px)", justifyContent: "center", gridGap: "30px", paddingTop: "50px"}}>
+            {data.map(data => (
+                <MakeCard 
+                    key={data.id}
+                    image={data.image}
+                    title={data.title}
+                    description={data.description}
+                    url={data.url}
+                />
+            ))}
+            </Grid>
         </>
     );
 }
