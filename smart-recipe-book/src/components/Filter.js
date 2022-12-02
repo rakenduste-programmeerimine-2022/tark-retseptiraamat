1b28f5b8570e8f2bcc856fa5b6cbae882eeef739
Reydan Ramon Niineorg <90023837+Reydan7@users.noreply.github.com>
2022-12-02 22:57:40 +0200

    search by name function

--- smart-recipe-book/src/components/Filter.js
+++ smart-recipe-book/src/components/Filter.js
@@ -1,72 +1,145 @@
-import react from 'react';
-import Button from '@mui/material/Button';
-// Filter search bar and search button
-// filter uses backend getRecipeByIngredient function from recipeControllers.js
-// adding engidients as tags with a add button and then clicking search will search for recipes with those ingredients
+import react from "react";
+import Button from "@mui/material/Button";
+import axios from "axios";
 
 const Filter = () => {
-    const [ingredient, setIngredient] = react.useState('');
-    const [ingredients, setIngredients] = react.useState([]);
-    const [recipes, setRecipes] = react.useState([]);
-    const [search, setSearch] = react.useState(false);
+  const [ingredient, setIngredient] = react.useState("");
+  const [ingredients, setIngredients] = react.useState([]);
+  const [recipes, setRecipes] = react.useState([]);
+  const [search, setSearch] = react.useState(false);
+  const [searchtype, setSearchType] = react.useState("name");
+  const [recipeName, setRecipeName] = react.useState("");
 
-    const addIngredient = () => {
-        setIngredients([...ingredients, ingredient]);
-        setIngredient('');
+  const addIngredient = () => {
+    if (ingredient !== "") {
+      setIngredients([...ingredients, ingredient]);
+      setIngredient("");
     }
+  };
 
-    const searchRecipes = () => {
-        setSearch(true);
+  const searchRecipes = () => {
+    if (ingredients.length > 0 || recipeName !== "") {
+      setSearch(true);
     }
+  };
 
-    const deleteIngredient = (index) => {
-        const newIngredients = [...ingredients];
-        newIngredients.splice(index, 1);
-        setIngredients(newIngredients);
+  const deleteIngredient = (index) => {
+    const newIngredients = [...ingredients];
+    newIngredients.splice(index, 1);
+    setIngredients(newIngredients);
+  };
+
+  react.useEffect(() => {
+    if (search && searchtype == "ingredient") {
+      axios
+        .get("http://localhost:5000/recipe/")
+        .then((res) => {
+          const recipes = res.data;
+          const newRecipes = [];
+          recipes.forEach((recipe) => {
+            let count = 0;
+            recipe.ingredients.forEach((recipeIngredient) => {
+              ingredients.forEach((ingredient) => {
+                if (
+                  recipeIngredient
+                    .toLowerCase()
+                    .includes(ingredient.toLowerCase())
+                ) {
+                  count++;
+                }
+              });
+            });
+            if (count > 0) {
+              newRecipes.push({ recipe: recipe, count: count });
+            }
+          });
+          newRecipes.sort((a, b) => b.count - a.count);
+          console.log(newRecipes);
+          setRecipes(newRecipes);
+        })
+        .catch((err) => console.log(err));
+    } else if (search && searchtype == "name") {
+      axios.get("http://localhost:5000/recipe/")
+        .then((res) => {
+            const recipes = res.data;
+            const newRecipes = [];
+            recipes.forEach((recipe) => {
+                if (recipeName.toLowerCase().includes(recipe.name.toLowerCase())) {
+                newRecipes.push(recipe);
+                }
+            });
+            setRecipes(newRecipes);
+            }
+        )
+        .catch((err) => console.log(err));
     }
+  }, [search]);
 
-    react.useEffect(() => {
-        if (search) {
-            fetch(`http://localhost:5000/recipe/ingredient/${ingredients}`)
-                .then(res => res.json())
-                .then(data => {
-                    setRecipes(data);
-                    setSearch(false);
-                })
-                .catch(err => console.log(err));
-        }
-    }, [search]);
+  return (
+    <div style={{ marginTop: "8vh" }}>
+      <div>
+        <Button onClick={() => setSearchType("name")}>Search by name</Button>
+        <Button onClick={() => setSearchType("ingredient")}>
+          search by ingredient
+        </Button>
+        {searchtype === "name" ? (
+          <>
+            <input type="text" placeholder="search by name" value={recipeName} onChange={(e) => setRecipeName(e.target.value)} />
+            <Button variant="contained" onClick={searchRecipes}>
+              Search
+            </Button>
 
-    return (
-        <div style={{marginTop:"8vh"}}>
+            {recipes.map((recipe) => (
+                <div>
+                    <h1>{recipe.name}</h1>
+                    <p>{recipe.description}</p>
+                </div>
+            ))}
+
+          </>
+        ) : (
+          <>
+            <input
+              type="text" placeholder="search by ingredient"
+              value={ingredient}
+              onChange={(e) => setIngredient(e.target.value)}
+            />
+            <Button variant="contained" onClick={addIngredient}>
+              Add
+            </Button>
             <div>
-                <input type="text" value={ingredient} onChange={(e) => setIngredient(e.target.value)} />
-                <Button variant="contained" onClick={addIngredient}>Add</Button>
+              {ingredients.map((ingredient, index) => (
+                <div key={index}>
+                  <p>{ingredient}</p>
+                  <Button
+                    variant="contained"
+                    onClick={() => deleteIngredient(index)}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              ))}
             </div>
             <div>
-                {ingredients.map((ingredient, index) => (
-                    <div key={index}>
-                        <p>{ingredient}</p>
-                        <Button variant="contained" onClick={() => deleteIngredient(index)}>Delete</Button>
-                    </div>
-                ))}
+              <Button variant="contained" onClick={searchRecipes}>
+                Search
+              </Button>
             </div>
             <div>
-                <Button variant="contained" onClick={searchRecipes}>Search</Button>
+              {recipes.map((recipe, index) => (
+                <div key={index}>
+                  <p>{recipe.recipe.name}</p>
+                  <p>{recipe.recipe.description}</p>
+                  <p>{recipe.recipe.ingredients}</p>
+                  <p>{recipe.recipe.instructions}</p>
+                </div>
+              ))}
             </div>
-            <div>
-                {recipes.map((recipe, index) => (
-                    <div key={index}>
-                        <p>{recipe.name}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
-}
-
-
-
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Filter;
-
